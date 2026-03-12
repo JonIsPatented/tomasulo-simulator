e69b5df04180248067194df6ef508552b211baf9
Conrad Falloff <radzo73qwerty@gmail.com>
2026-03-11 23:56:16 -0400

    Adjusted spacings and CSS

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,7 @@ import { Navbar } from './components/Navbar'
 export const App = () => {
     return (
         <div className='app-wrapper'>
-            <Navbar title={"Test"}>Test</Navbar>
-            <h1>Tomasulo's Algorithm Simulator</h1>
+            <Navbar title={"Tomasulo's Algorithm Simulator"}>Test</Navbar>
             <CollapsibleContainer title={<h3>Example Container</h3>}>
                 <h3>Example of a Container</h3>
                 <p>This is an example of a collapsible container.</p>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,6 +8,12 @@ interface NavbarProps{
 
 export const Navbar = ({title, children}:NavbarProps) => {
     return (
-        <div className="NavbarMain"><h2>{title}</h2></div>
+        <div className='NavbarMain'>
+            <h3>icon</h3>
+            <div></div>
+            <h2>{title}</h2>
+            <div></div>
+            <h3>clock</h3>
+        </div>
     )
 }
