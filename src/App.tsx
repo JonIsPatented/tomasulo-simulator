f3a5dbe98f9a599fa16a91b01962f872986c83a2
Conrad Falloff <radzo73qwerty@gmail.com>
2026-03-17 11:07:43 -0400

    Add Clock/Time slider and number box

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import './App.css'
 import { CollapsibleContainer } from './components/CollapsibleContainer'
 import { Navbar } from './components/Navbar'
+import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+
+        
 
 export const App = () => {
     return (
-        <div className='app-wrapper'>
-            <Navbar title={"Tomasulo's Algorithm Simulator"}>Test</Navbar>
-            <CollapsibleContainer title={<h3>Example Container</h3>}>
-                <h3>Example of a Container</h3>
-                <p>This is an example of a collapsible container.</p>
-            </CollapsibleContainer>
-        </div>
+        <PrimeReactProvider value={{}}>
+            <div className='app-wrapper'>
+                <Navbar title={"Tomasulo's Algorithm Simulator"}>Test</Navbar>
+                <CollapsibleContainer title={<h3>Example Container</h3>}>
+                    <h3>Example of a Container</h3>
+                    <p>This is an example of a collapsible container.</p>
+                </CollapsibleContainer>
+            </div>
+        </PrimeReactProvider>
     )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,9 @@
-import type { ReactNode } from 'react'
+import { useState, type ReactNode } from 'react'
+import { Slider } from 'primereact/slider';
 import './Navbar.css'
+import { InputNumber, type InputNumberValueChangeEvent } from 'primereact/inputnumber';
+
+        
 
 interface NavbarProps{
     title: ReactNode,
@@ -7,13 +11,35 @@ interface NavbarProps{
 }
 
 export const Navbar = ({title, children}:NavbarProps) => {
+    const [value, setValue] = useState(1);
+
     return (
         <div className='NavbarMain'>
             <h3>icon</h3>
-            <div></div>
+            <div className='NavbarSpacer'></div>
             <h2>{title}</h2>
-            <div></div>
-            <h3>clock</h3>
+            <div className='NavbarSpacer'></div>
+            <div className='Clock'>
+                <h4>Time</h4>
+                <div className="card flex justify-content-center">
+                    <Slider 
+                        value={value} 
+                        onChange={(e) => setValue(e.value)} 
+                        className="w-14rem"
+                        min={1}
+                        max={10}
+
+                    />
+                    <InputNumber 
+                        value={value} 
+                        onValueChange={(e: InputNumberValueChangeEvent) => setValue(e.value)} 
+                        mode="decimal"
+                        showButtons
+                        min={1}
+                        max={10}
+                    />
+                </div>
+            </div>
         </div>
     )
 }
