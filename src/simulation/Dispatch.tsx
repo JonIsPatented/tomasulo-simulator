851cace8d9b673e155f7383355f6cb050cae2433
Jon Cole <jonispatented@gmail.com>
2026-03-24 20:11:32 -0400

    added a way for registers to indicate that they are waiting for a register load signal instead of another reservation station

--- src/simulation/Dispatch.tsx
+++ src/simulation/Dispatch.tsx
@@ -4,6 +4,46 @@ export const dispatchStep = (currentState: SimulatorData) => {
 
     const bus = currentState.commonDataBus
 
+    let resStations = currentState.reservationStations
+    let regFile = currentState.registerFile
+
+    if (bus.value !== null) {
+        if (bus.sourceStation !== null) {
+            resStations = resStations.map((station) => {
+                return {
+                    ...station,
+                    firstArgumentValue:
+                        station.firstArgumentStation === bus.sourceStation ?
+                            bus.value : station.firstArgumentValue,
+                    secondArgumentValue:
+                        station.secondArgumentStation === bus.sourceStation ?
+                            bus.value : station.secondArgumentValue,
+                }
+            })
+        }
+        else if (bus.destinationRegister !== null) {
+            resStation = resStation.map((station) => {
+                return {
+                    ...station,
+                    firstArgumentValue:
+                        station.firstArgumentWaitingRegister === bus.destinationRegister ?
+                            bus.value : station.firstArgumentValue,
+                    secondArgumentValue:
+                        station.secondArgumentWaitingRegister === bus.destinationRegister ?
+                            bus.value : station.secondArgumentValue,
+                }
+            })
+            regFile = regFile.map((register, i) => {
+                return {
+                    ...register,
+                    value:
+                        i === bus.destinationRegister ?
+                            bus.value : register.value
+                }
+            })
+        }
+    }
+
     return {
         ...currentState
     }
