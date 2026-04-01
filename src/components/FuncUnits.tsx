538764b0f0dc26fdcf06c2e04ef5a88f153a7553
JamesBennett5027 <jameslbennett2004@gmail.com>
2026-03-31 20:08:59 -0400

    formated Source

--- src/components/FuncUnits.tsx
+++ src/components/FuncUnits.tsx
@@ -1,18 +1,19 @@
 import {Flex,Grid,Text} from "@radix-ui/themes";
 import { useSimulation } from "../hooks/useSimulation";
 import { type FunctionUnit } from "../simulation/Simulation"; 
+
 const FuncUnitGroup = ({title,units,addOrMultiply}:{title :string,units:Array<FunctionUnit>,addOrMultiply:String}) => {
     const index = 0
     return (
     <Flex direction="column">
         <Text size="2" weight="bold">{title}</Text>
       <Grid columns="6" gap="2" className="items-center text-sm whitespace-nowrap">
-            <Text size="1" weight="bold" color="gray">name</Text>
+            <Text size="1" weight="bold" color="gray">Name</Text>
             <Text size="1" weight="bold" color="gray">Op</Text>
             <Text size="1" weight="bold" color="gray">Arg1</Text>
             <Text size="1" weight="bold" color="gray">Arg2</Text>
-            <Text size="1" weight="bold" color="gray">TL</Text>
-            <Text size="1" weight="bold" color="gray">sourceReg</Text>
+            <Text size="1" weight="bold" color="gray" wrap="wrap">Cycles left</Text>
+            <Text size="1" weight="bold" color="gray">Source</Text>
         {units.map((i)=>(
             <FuncUnitRows unit={i} addOrMull={addOrMultiply} Index={index}/>
         ))}
@@ -21,7 +22,9 @@ const FuncUnitGroup = ({title,units,addOrMultiply}:{title :string,units:Array<Fu
 )
 }
 const FuncUnitRows = ({unit,addOrMull,Index}:{unit:FunctionUnit,addOrMull:String,Index:Number}) =>{
-    const name = addOrMull +Index.toString()
+    
+    const name = addOrMull +Index.toString();
+
     return(
     <>
         <Text className="border-b border-gray-100 py-1">{name}</Text>
@@ -29,18 +32,18 @@ const FuncUnitRows = ({unit,addOrMull,Index}:{unit:FunctionUnit,addOrMull:String
         <Text className="border-b border-gray-100 py-1">{unit.firstArgumentValue ?? "-"}</Text>
         <Text className="border-b border-gray-100 py-1">{unit.secondArgumentValue ?? "-"}</Text>
         <Text className="border-b border-gray-100 py-1">{unit.ticksLeft ?? "-"}</Text>
-        <Text className="border-b border-gray-100 py-1">{unit.sourceReservationStation ?? "-"}</Text>
+       <Text className="border-b border-gray-100 py-1">{unit.sourceReservationStation ? `${addOrMull}${unit.sourceReservationStation}`: "-"}</Text>
+        
     </>
-    
     )
 }
 export const FuncUnits = () => {
     const [MDF, ASF] = useSimulation((data) => [data.multiplyDivideFunctionUnits, data.addSubtractFunctionUnits])
     return (
 
-        <Grid columns="2" gap="9">
-            <FuncUnitGroup title="add or subtract" units={ASF}   addOrMultiply="add"/>
-            <FuncUnitGroup title="multipy or devide" units={MDF} addOrMultiply="mul"/>
+        <Grid columns="2" gap="2">
+            <FuncUnitGroup title="Add/Subtract" units={ASF}   addOrMultiply="add"/>
+            <FuncUnitGroup title="Mul/Dev" units={MDF} addOrMultiply="mul"/>
         </Grid>
     )
 }
