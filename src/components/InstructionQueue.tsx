4de506bb527b08882307bebea38f0c744b129b47
OfficeCoffee <rosetaylor45342@gmail.com>
2026-04-01 01:18:53 -0400

    Changing the opcodes to be lowercase caused the program to break because the opcodes were still capitalized in issue.tsx. Changed the opcodes in Issue.tsx to be lowercase. Changed the InstructionQueue component so it will render memory instructions and ALU instructions differently. '

--- src/components/InstructionQueue.tsx
+++ src/components/InstructionQueue.tsx
@@ -3,11 +3,23 @@ import { useSimulation } from "../hooks/useSimulation"
 
 export const InstructionQueue = () => {
     const queueData = useSimulation((data) => data.instructionQueue)
+
     return (
         <Flex direction="column">
-            {queueData.map(instr => {
+            {queueData.map((instr, i) => {
+                // Render a memory instruction if it contains a base register, otherwise render an ALU instruction
+                if ('baseRegister' in instr) {
+                    return (
+                        <Flex key={`instr-${i}`} direction="row" gap="2">
+                            <div>{instr.opcode}</div>
+                            <div>f{instr.register}</div>
+                            <div>{instr.offset}(f{instr.baseRegister})</div>
+                        </Flex>
+                    )
+                }
+
                 return (
-                    <Flex direction="row" gap="2">
+                    <Flex key={`instr-${i}`} direction="row" gap="2">
                         <div>{instr.opcode}</div>
                         <div>f{instr.source1}</div>
                         <div>f{instr.source2}</div>
