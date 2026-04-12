36ef23ae8965b4c1b8bd49dfde0ff0ba85e754a9
Jon Cole <jonispatented@gmail.com>
2026-04-12 14:47:38 -0400

    Updated the branch to use the new type system.

--- src/components/FuncUnits.tsx
+++ src/components/FuncUnits.tsx
@@ -1,55 +1,137 @@
-import {Flex,Grid,Text} from "@radix-ui/themes";
-import { useSimulation } from "../hooks/useSimulation";
-import { type FunctionUnit } from "../simulation/Simulation"; 
+import { Flex, Grid, Text } from '@radix-ui/themes'
+import { useSimulation } from '../hooks/useSimulation'
+import { type FunctionUnitData } from '../simulation/Simulation'
 
-const FuncUnitGroup = ({title,units,addOrMultiply}:{title :string,units:Array<FunctionUnit>,addOrMultiply:String}) => {
-    return (
-    <Flex direction="column">
-        <Text size="2" weight="bold">{title}</Text>
-      <Grid columns="5" gap="2" className="items-center text-sm whitespace-nowrap">
-            
-            <Text size="1" weight="bold" color="gray">Op</Text>
-            <Text size="1" weight="bold" color="gray">Arg1</Text>
-            <Text size="1" weight="bold" color="gray">Arg2</Text>
-            <Text size="1" weight="bold" color="gray" wrap="wrap">Cycles left</Text>
-            <Text size="1" weight="bold" color="gray">Source</Text>
-        {units.map((i)=>(
-            <FuncUnitRows unit={i} addOrMull={addOrMultiply}/>
+const FuncUnitGroup = ({
+  title,
+  units,
+  type,
+}: {
+  title: string
+  units: Array<FunctionUnitData<number>>
+  type: 'add' | 'mul'
+}) => {
+  return (
+    <Flex direction='column'>
+      <Text
+        size='2'
+        weight='bold'
+      >
+        {title}
+      </Text>
+      <Grid
+        columns='5'
+        gap='2'
+        className='items-center text-sm whitespace-nowrap'
+      >
+        <Text
+          size='1'
+          weight='bold'
+          color='gray'
+        >
+          Op
+        </Text>
+        <Text
+          size='1'
+          weight='bold'
+          color='gray'
+        >
+          Arg1
+        </Text>
+        <Text
+          size='1'
+          weight='bold'
+          color='gray'
+        >
+          Arg2
+        </Text>
+        <Text
+          size='1'
+          weight='bold'
+          color='gray'
+          wrap='wrap'
+        >
+          Ticks Left
+        </Text>
+        <Text
+          size='1'
+          weight='bold'
+          color='gray'
+        >
+          Source
+        </Text>
+        {units.map((i) => (
+          <FuncUnitRows
+            unit={i}
+            type={type}
+          />
         ))}
-    </Grid>
-</Flex>
-)
+      </Grid>
+    </Flex>
+  )
 }
-const FuncUnitRows = ({unit,addOrMull}:{unit:FunctionUnit,addOrMull:String}) =>{
-   const addrResCount = useSimulation((data) => data.adderReservationStationCount)
-   const soruseValue = unit.sourceReservationStation != null ? `${addOrMull}${addOrMull==="mul"?unit.sourceReservationStation-addrResCount:unit.sourceReservationStation}`:"-"
-    return(
+const FuncUnitRows = ({
+  unit,
+  type,
+}: {
+  unit: FunctionUnitData<number>
+  type: 'add' | 'mul'
+}) => {
+  const adderReservationStationCount = useSimulation(
+    (data) => data.adderReservationStationCount
+  )
+  const sourceValue = !unit.isEmpty
+    ? `${type}${
+        type === 'mul'
+          ? unit.sourceStationIndex -
+            adderReservationStationCount
+          : unit.sourceStationIndex
+      }`
+    : '-'
+  return (
     <>
-        <Text className="border-b border-gray-100 py-1">{unit.operation ?? "-"}</Text>
-        <Text className="border-b border-gray-100 py-1">{unit.firstArgumentValue ?? "-"}</Text>
-        <Text className="border-b border-gray-100 py-1">{unit.secondArgumentValue ?? "-"}</Text>
-        <Text className="border-b border-gray-100 py-1">{unit.ticksLeft ?? "-"}</Text>
-        <Text className="border-b border-gray-100 py-1">{soruseValue}</Text>
-        
+      <Text className='border-b border-gray-100 py-1'>
+        {unit.isEmpty ? '-' : unit.operation}
+      </Text>
+      <Text className='border-b border-gray-100 py-1'>
+        {unit.isEmpty ? '-' : unit.firstArgument}
+      </Text>
+      <Text className='border-b border-gray-100 py-1'>
+        {unit.isEmpty ? '-' : unit.secondArgument}
+      </Text>
+      <Text className='border-b border-gray-100 py-1'>
+        {unit.isEmpty ? '-' : unit.ticksLeft}
+      </Text>
+      <Text className='border-b border-gray-100 py-1'>
+        {sourceValue}
+      </Text>
     </>
-    )
+  )
 }
 export const FuncUnits = () => {
-    const {
-        mulDivFuncUnits,
-        addSubFuncUnits,
-    } = useSimulation((data) => {
-        return {
-            mulDivFuncUnits: data.multiplyDivideFunctionUnits,
-            addSubFuncUnits: data.addSubtractFunctionUnits,
-        }
+  const { mulDivFuncUnits, addSubFuncUnits } =
+    useSimulation((data) => {
+      return {
+        mulDivFuncUnits: data.multiplyDivideFunctionUnits,
+        addSubFuncUnits: data.addSubtractFunctionUnits,
+      }
     })
 
-    return (
-
-        <Grid columns="2" gap="2">
-            <FuncUnitGroup title="Add/Subtract" units={ASF}   addOrMultiply="add"/>
-            <FuncUnitGroup title="Mul/Dev" units={MDF} addOrMultiply="mul"/>
-        </Grid>
-    )
+  return (
+    <Grid
+      columns='2'
+      gap='2'
+    >
+      <FuncUnitGroup
+        title='Add/Subtract'
+        units={addSubFuncUnits}
+        type='add'
+      />
+      <FuncUnitGroup
+        title='Mul/Dev'
+        units={mulDivFuncUnits}
+        type='mul'
+      />
+    </Grid>
+  )
 }
