f625bd2e7e63c97ece7940e2f30164ec6b4a3832
Jon Cole <jonispatented@gmail.com>
2026-04-14 16:36:59 -0400

    fixed the NumberInput field to act more predictably

--- src/components/NumberInput.tsx
+++ src/components/NumberInput.tsx
@@ -1,29 +1,43 @@
-interface NumberInputProps{
-    minValue: number
-    maxValue: number
-    value: number
-    onChange: (value: number) => void
-    disabled: boolean
+import { useState } from 'react'
+
+interface NumberInputProps {
+  minValue: number
+  maxValue: number
+  value: number
+  onChange: (value: number) => void
+  disabled: boolean
 }
 
-export const NumberInput = ({minValue, maxValue, value, onChange, disabled}: NumberInputProps) => {
-    return (
-        <input
-                type="number"
-                name="quantity"
-                min= {minValue}
-                max= {maxValue}
-                value={value}
-                onChange={(e) => {
-                    //I love having to manually sanitize
-                    if (e.currentTarget.value == "") {
-                        e.currentTarget.value = minValue.toString()
-                    }
-                    const clamped: number = Math.min(Math.max(parseInt(e.currentTarget.value), minValue), maxValue)
-                    e.currentTarget.value = clamped.toString()
-                    onChange(clamped)
-                }}
-                disabled={disabled}
-            />
+export const NumberInput = ({
+  minValue,
+  maxValue,
+  value,
+  onChange,
+  disabled,
+}: NumberInputProps) => {
+  const [isEmpty, setIsEmpty] = useState(false)
+
+  return (
+    <input
+      type='number'
+      min={minValue}
+      max={maxValue}
+      value={isEmpty ? '' : value}
+      onChange={(e) => {
+        if (e.currentTarget.value == '') {
+          setIsEmpty(true)
+          return
+        } else setIsEmpty(false)
+        const clamped: number = Math.min(
+          Math.max(parseInt(e.currentTarget.value), minValue),
+          maxValue
         )
+        onChange(clamped)
+      }}
+      onBlur={() => {
+        setIsEmpty(false)
+      }}
+      disabled={disabled}
+    />
+  )
 }
