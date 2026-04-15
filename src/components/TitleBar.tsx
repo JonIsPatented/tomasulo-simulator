fc2a5180db099142de77601e765fc499bd864418
JamesBennett5027 <jameslbennett2004@gmail.com>
2026-04-15 18:14:29 -0400

    logic is there just need to figure out how to add to the instruction queue

--- src/components/TitleBar.tsx
+++ src/components/TitleBar.tsx
@@ -1,13 +1,30 @@
 import { Button, Dialog, Flex, Grid, Heading, Slider, Text } from "@radix-ui/themes"
 import { useSimulation } from '../hooks/useSimulation'
-import { Simulation } from '../simulation/Simulation'
-
+import { Simulation, type Instruction } from '../simulation/Simulation'
+import { useState } from "react"
+import {assembleProgram} from "../simulation/Assemble"
 
 export const TitleBar = () => {
-
+    const [code,setcode]= useState("");
     const clockRate = useSimulation((data) => data.clockRate)
     const simulation = Simulation.getSimulation()
+    const RegFileSize = useSimulation((data)=> data.registerFile.length);
+    
+    function assemble(){
+        const codeString:string = code;
+        
+        const ParsedInstructions = assembleProgram(codeString,RegFileSize);
+       if(ParsedInstructions.ok){
+        
 
+        for(let i = 0; i< ParsedInstructions.value.length;i++){
+           console.log(ParsedInstructions.value[i]);
+        }
+      
+       }else{
+        console.log(ParsedInstructions.error)
+       }
+}
     return (
         <Grid columns="3">
             <Flex justify="start" align="center">
@@ -26,6 +43,13 @@ export const TitleBar = () => {
                                 min={1}
                                 max={10}
                             />
+                            <div className="flex flex-col gap-2">
+                                <Button onClick={() => assemble()}>import code</Button>
+                            <label>Import Code:</label>
+                            <textarea className="w-full h-40 border rounded p-2" 
+                            placeholder="Paste your code here..." 
+                            value={code} onChange={(input)=>setcode(input.target.value)}/>
+                            </div>
                         </div>
                     </Dialog.Content>
                 </Dialog.Root>
