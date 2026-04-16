664b7cf56f7ed4e3b4e378a7ce73d455e6d107d3
Jon Cole <jonispatented@gmail.com>
2026-04-16 17:07:10 -0400

    began work on the memory unit frontend component

--- src/components/MemoryUnit.tsx
+++ src/components/MemoryUnit.tsx
@@ -1,6 +1,75 @@
+import { Grid } from '@radix-ui/themes/dist/cjs/index.js'
+import { useSimulation } from '../hooks/useSimulation'
+import { useState } from 'react'
+import { Text, TextField } from '@radix-ui/themes'
+
 export const MemoryUnit = () => {
-    return (
+  const memoryUnit = useSimulation((data) => data.memoryUnit)
+  const [startIndex] = useState(0)
+  const currentlyVisibleAddresses = Array.from(
+    { length: 10 },
+    (_, i) => i + startIndex
+  )
+  return (
+    <Grid
+      columns='2'
+      gapX='2'
+      className='items-center text-sm'
+    >
+      {/* Header */}
+      <Text
+        size='1'
+        weight='bold'
+        color='gray'
+      >
+        Address
+      </Text>
+      <Text
+        size='1'
+        weight='bold'
+        align='center'
+        color='gray'
+      >
+        Value
+      </Text>
+
+      {/* Visible Addresses */}
+      {currentlyVisibleAddresses.map((address, i) => (
         <>
+          <Text
+            key={`address-${i}`}
+            weight='medium'
+            className='border-b border-gray-100 py-1 whitespace-nowrap'
+          >
+            {address}
+          </Text>
+          <TextField.Root
+            key={`memory-value-${i}`}
+            size='1'
+            value={memoryUnit.get(address) ?? 0}
+            readOnly
+            className='text-right'
+          />
         </>
-    )
+      ))}
+    </Grid>
+
+    // <Table.Root>
+    //   <Table.Header>
+    //     <Table.Row>
+    //       <Table.ColumnHeaderCell>Address</Table.ColumnHeaderCell>
+    //       <Table.ColumnHeaderCell>Value</Table.ColumnHeaderCell>
+    //     </Table.Row>
+    //   </Table.Header>
+
+    //   <Table.Body>
+    //     {currentlyVisibleAddresses.map((address) => (
+    //       <Table.Row>
+    //         <Table.Cell>{address}</Table.Cell>
+    //         <Table.Cell>{memoryUnit.get(address) ?? 0}</Table.Cell>
+    //       </Table.Row>
+    //     ))}
+    //   </Table.Body>
+    // </Table.Root>
+  )
 }
