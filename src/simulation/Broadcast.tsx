1f8f394e2c0a592c72fef8f3c4450ade5887027d
Jon Cole <jonispatented@gmail.com>
2026-04-21 23:05:36 -0400

    format

--- src/simulation/Broadcast.tsx
+++ src/simulation/Broadcast.tsx
@@ -1,4 +1,4 @@
-import type { ArithmeticOpcode, SimulatorData } from "./Simulation"
+import type { ArithmeticOpcode, SimulatorData } from './Simulation'
 
 // ⋆.˚⟡ ࣪ ˖ MEMORY IS A LIE ⋆.˚⟡ ࣪ ˖
 // for now...
@@ -6,82 +6,90 @@ import type { ArithmeticOpcode, SimulatorData } from "./Simulation"
 // Computes the result for each type of operation
 // Currently does not support load / store
 const computeResult = (
-    operation: ArithmeticOpcode,
-    firstArgumentValue: number,
-    secondArgumentValue: number
+  operation: ArithmeticOpcode,
+  firstArgumentValue: number,
+  secondArgumentValue: number
 ): number => {
-    switch (operation) {
-        case 'add':
-            return firstArgumentValue + secondArgumentValue
-        case 'sub':
-            return firstArgumentValue - secondArgumentValue
-        case 'mul':
-            return firstArgumentValue * secondArgumentValue
-        case 'div':
-            return secondArgumentValue === 0 ? NaN : firstArgumentValue / secondArgumentValue
-    }
+  switch (operation) {
+    case 'add':
+      return firstArgumentValue + secondArgumentValue
+    case 'sub':
+      return firstArgumentValue - secondArgumentValue
+    case 'mul':
+      return firstArgumentValue * secondArgumentValue
+    case 'div':
+      return secondArgumentValue === 0
+        ? NaN
+        : firstArgumentValue / secondArgumentValue
+  }
 }
 
-export const broadcastStep = (currentState: SimulatorData): SimulatorData => {
-    const multiplyFunctionUnit = currentState.multiplyDivideFunctionUnits[0]
-    const addFunctionUnit = currentState.addSubtractFunctionUnits[0]
+export const broadcastStep = (
+  currentState: SimulatorData
+): SimulatorData => {
+  const multiplyFunctionUnit =
+    currentState.multiplyDivideFunctionUnits[0]
+  const addFunctionUnit = currentState.addSubtractFunctionUnits[0]
 
-    // Checks if the mul / div function unit is done
-    if (!multiplyFunctionUnit.isEmpty && multiplyFunctionUnit.ticksLeft === 0) {
-        return {
-            ...currentState,
-            commonDataBus: {
-                isActive: true,
-                value: computeResult(
-                    multiplyFunctionUnit.operation,
-                    multiplyFunctionUnit.firstArgument,
-                    multiplyFunctionUnit.secondArgument,
-                ),
-                source: 'operation',
-                sourceStation: multiplyFunctionUnit.sourceStationIndex,
-            },
-            multiplyDivideFunctionUnits: [
-                {
-                    isEmpty: true
-                }
-            ],
-            transmitFlags: {
-                ...currentState.transmitFlags,
-                functionUnitsToCommonDataBus: true
-            },
-        }
-    }
-
-    // Checks if the add / sub function unit is done
-    if (!addFunctionUnit.isEmpty && addFunctionUnit.ticksLeft === 0) {
-        return {
-            ...currentState,
-            commonDataBus: {
-                isActive: true,
-                value: computeResult(
-                    addFunctionUnit.operation,
-                    addFunctionUnit.firstArgument,
-                    addFunctionUnit.secondArgument
-                ),
-                source: 'operation',
-                sourceStation: addFunctionUnit.sourceStationIndex,
-            },
-            addSubtractFunctionUnits: [
-                {
-                    isEmpty: true
-                }
-            ],
-            transmitFlags: {
-                ...currentState.transmitFlags,
-                functionUnitsToCommonDataBus: true
-            },
-        }
-    }
-
+  // Checks if the mul / div function unit is done
+  if (
+    !multiplyFunctionUnit.isEmpty &&
+    multiplyFunctionUnit.ticksLeft === 0
+  ) {
     return {
-        ...currentState,
-        commonDataBus: {
-            isActive: false
-        }
+      ...currentState,
+      commonDataBus: {
+        isActive: true,
+        value: computeResult(
+          multiplyFunctionUnit.operation,
+          multiplyFunctionUnit.firstArgument,
+          multiplyFunctionUnit.secondArgument
+        ),
+        source: 'operation',
+        sourceStation: multiplyFunctionUnit.sourceStationIndex,
+      },
+      multiplyDivideFunctionUnits: [
+        {
+          isEmpty: true,
+        },
+      ],
+      transmitFlags: {
+        ...currentState.transmitFlags,
+        functionUnitsToCommonDataBus: true,
+      },
     }
+  }
+
+  // Checks if the add / sub function unit is done
+  if (!addFunctionUnit.isEmpty && addFunctionUnit.ticksLeft === 0) {
+    return {
+      ...currentState,
+      commonDataBus: {
+        isActive: true,
+        value: computeResult(
+          addFunctionUnit.operation,
+          addFunctionUnit.firstArgument,
+          addFunctionUnit.secondArgument
+        ),
+        source: 'operation',
+        sourceStation: addFunctionUnit.sourceStationIndex,
+      },
+      addSubtractFunctionUnits: [
+        {
+          isEmpty: true,
+        },
+      ],
+      transmitFlags: {
+        ...currentState.transmitFlags,
+        functionUnitsToCommonDataBus: true,
+      },
+    }
+  }
+
+  return {
+    ...currentState,
+    commonDataBus: {
+      isActive: false,
+    },
+  }
 }
